114baf0ac6846be1f189442c9bc6357475aadb7f
mielesantos <mielesnts@gmail.com>
2022-12-10 21:14:14 -0300

    adicionando templates html e index

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-
+from fastapi.requests import Request
 
 app = FastAPI(docs_url=None, redoc_url=None)
 
@@ -10,9 +10,16 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/media", StaticFiles(directory="media"), name="media")
 
 
+@app.get("/")
+async def index(request: Request):
+    context = {"request": request}
+
+    return templates.TemplateResponse("home/index.html", context=context)
+
+
 if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        app="main:app", host="0.0.0.0", port=8000, log_level="info", reload=True
+        app="main:app", host="0.0.0.0", port=8001, log_level="info", reload=True
     )
